b1b5d03646185eaec207c0dc7fdea8c537c79d18
Sooheongod02 <c32456998@gmail.com>
2025-11-28 18:11:14 +0900

    Update tf_static.launch.py

--- tf_static.launch.py
+++ tf_static.launch.py
@@ -1,32 +1,26 @@
-from setuptools import setup
-import os
-from glob import glob
+from launch import LaunchDescription
+from launch_ros.actions import Node
 
-package_name = 'my_sensor_launch'
+def generate_launch_description():
+    return LaunchDescription([
+        # 1. OT128 (지붕 위) 위치 정의
+        # base_link -> ot128_frame
+        # 위치: x=0m(중앙), y=0m(중앙), z=1.8m(높이)
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='ot128_tf',
+            arguments=['0', '0', '1.8', '0', '0', '0', 'base_link', 'ot128_frame']
+        ),
 
-setup(
-    name=package_name,
-    version='0.0.0',
-    packages=[package_name],
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        
-        # --- [여기부터 추가하세요] ---
-        # launch 폴더 안의 모든 .launch.py 파일을 설치 경로로 복사한다는 뜻입니다.
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-        # -------------------------
-    ],
-    install_requires=['setuptools'],
-    zip_safe=True,
-    maintainer='user',
-    maintainer_email='user@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
-    entry_points={
-        'console_scripts': [
-        ],
-    },
-)
+        # 2. QT128 (전면 범퍼) 위치 정의
+        # base_link -> qt128_frame
+        # 위치: x=1.5m(앞으로), y=0m, z=0.5m(높이) / 약간 아래를 봄(pitch)
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='qt128_tf',
+            # arguments 순서: x y z yaw pitch roll parent child
+            arguments=['1.5', '0', '0.5', '0', '0', '0', 'base_link', 'qt128_frame']
+        )
+    ])
